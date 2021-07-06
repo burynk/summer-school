37bcaa0bb6b0e3775ba9c1c65696f420193b91fe
burynk <40724806+burynk@users.noreply.github.com>
2021-07-06 21:59:31 +0300

    uhhhhhh
    i don't care anymore, have a nice day

--- change_owner.sh
+++ change_owner.sh
@@ -1,8 +1,14 @@
 #!/bin/bash 
 
-if id "$1" >/dev/null 2>&1; then
-	chown -Rv $1 $2;
-	echo "success";
+if id "$1" >/dev/null 2>&1; 
+	then
+	if [ -d $2 ] 
+		then
+		chown -Rv $1 $2;
+		echo "success";
+		else
+		echo "sraka dupa this is not a folder, heh"
+	fi
 else
 	echo "beep boop error!!!";
 fi
